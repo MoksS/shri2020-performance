29342d14726f4f13f48c4730828dc676a168eaa7
MoksS <MoksShuv@yandex.ru>
2020-04-08 15:14:58 +0300

    ну рожаай

--- js/main.js
+++ js/main.js
@@ -32,4 +32,4 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function () {
         document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
     })
-});
+}, false);
