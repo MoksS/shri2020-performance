95ea35964d3d4d248bf9aac2ad00b73f3695938d
MoksS <MoksShuv@yandex.ru>
2020-04-08 19:25:53 +0300

    remove root

--- js/main.js
+++ js/main.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const buttonsContainer = document.querySelector(".buttons-wrap");
-    const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
-    const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
-    const purchaseListContainer = document.querySelector(".purchase-list-wrap");
-
-    confirmPurchaseButton.onclick = () => {
-        fridgeInfoContainer.remove()
-        buttonsContainer.style.display = "none";
-        purchaseListContainer.style.display = "block";
-    }
-
-    document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function () {
-        document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
-    })
-});
+document.addEventListener("DOMContentLoaded",()=>{const buttonsContainer=document.querySelector(".buttons-wrap");const fridgeInfoContainer=document.querySelector(".card_size_m:nth-child(8) .card-description");const confirmPurchaseButton=document.querySelector(".buttons-wrap .button_yellow");const purchaseListContainer=document.querySelector(".purchase-list-wrap");confirmPurchaseButton.onclick=()=>{fridgeInfoContainer.remove();buttonsContainer.style.display="none";purchaseListContainer.style.display="block"};document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click",function(){document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")})})
